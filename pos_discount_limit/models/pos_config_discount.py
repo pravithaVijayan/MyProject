6f43d1d1d7d499077fc9c82a143922c0d9510699
pravitha <c.pravitha.vijayan@gmail.com>
2021-11-25 19:41:33 +0530

    [Add]pos_discount_limit

--- pos_discount_limit/models/pos_config_discount.py
+++ pos_discount_limit/models/pos_config_discount.py
@@ -10,5 +10,12 @@ class ProductTemplate(models.Model):
     is_discount_limit = fields.Boolean(string="Discount Limit")
     discount_limit = fields.Integer(string='Limit',
                                     help="Limit used in the Point of Sale.")
-    # non_discount_categ_ids = fields.Many2many(comodel_name= 'pos.category',
-    #                                           string="discount")
+    non_discount_categ_ids = fields.Many2many('pos.category', 'reference',
+                                              string="discount")
+
+
+class NonDiscountCategory(models.Model):
+    _name = 'pos.category.nondiscount'
+
+    categ_ids = fields.Many2many('pos.category', string="Categories")
+    reference = fields.Many2one('pos.config')
--- pos_discount_limit/static/src/js/dis_limit.js
+++ pos_discount_limit/static/src/js/dis_limit.js
@@ -8,23 +8,43 @@ odoo.define('pos_discount_limit.receipt', function(require){
 
     var _super_orderline = models.Orderline.prototype;
     models.Orderline = models.Orderline.extend({
+            set_discount: function(discount) {
+                var category = this.get_product().pos_categ_id[0]
+                var non_discount_category = this.pos.config.non_discount_categ_ids
+                var flag = false
+//                console.log(category)
+                for (var categ in non_discount_category){
+//                console.log(category,non_discount_category[categ])
+                    if (category == non_discount_category[categ]){
+                        flag = true
+                    }
+                }
+//                console.log(flag)
+                if(flag == false){
+                    var parsed_discount = isNaN(parseFloat(discount)) ? 0 : field_utils.parse.float('' + discount);
+                    var disc = Math.min(Math.max(parsed_discount || 0, 0),100);
+                    this.discount = disc;
+                    var old_discountStr = this.discountStr
+                    this.discountStr = '' + disc;
+                    if (this.discountStr < this.pos.config.discount_limit){
+                       this.trigger('change',this);
+            //           console.log(this.discountStr)
+                       console.log("pos",this.get_product().pos_categ_id[0])
+                       console.log("category",this.pos.config.non_discount_categ_ids)
+                    }
+                    else{
+                        this.discountStr = old_discountStr
+                        this.discount = old_discountStr
+                        alert("Discount Limit Exceeded")
+                    }
+                }
+                else{
+                    alert("Discount Not Allowed for this Product Category")
+                    flag = false
+                }
+            }
 
-        set_discount: function(discount) {
-        var parsed_discount = isNaN(parseFloat(discount)) ? 0 : field_utils.parse.float('' + discount);
-        var disc = Math.min(Math.max(parsed_discount || 0, 0),100);
-        this.discount = disc;
-        var old_discountStr = this.discountStr
-        this.discountStr = '' + disc;
-        if (this.discountStr < this.pos.config.discount_limit){
-           this.trigger('change',this);
-           console.log(this.discountStr)
-           console.log(this.pos.config.discount_limit)
-        }
-        else{
-        this.discountStr = old_discountStr
-        alert("Discount Limit Exceeded")
-        }
-        },
+
         });
 
 });
