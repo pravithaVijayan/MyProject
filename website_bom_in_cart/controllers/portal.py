39a5ccc73aaa218b5363d7e1e0494f50c47d92bf
pravitha <c.pravitha.vijayan@gmail.com>
2021-11-30 22:01:02 +0530

    [Add]website_bom_in_cart

--- website_bom_in_cart/controllers/portal.py
+++ website_bom_in_cart/controllers/portal.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
-# -*- coding: utf-8 -*-
-from addons.website_sale.controllers.main import WebsiteSale
+
 from odoo import http, fields
 from odoo.http import request
+from odoo.tools.safe_eval import json
 
 
 class WebsiteSale(http.Controller):
@@ -20,7 +19,8 @@ class WebsiteSale(http.Controller):
 
         value = order._cart_update(product_id=product_id, line_id=line_id,
                                    add_qty=add_qty, set_qty=set_qty)
-        print(value)
+
+        # print(value)
         if not order.cart_quantity:
             request.website.sale_reset()
             return value
@@ -42,3 +42,56 @@ class WebsiteSale(http.Controller):
             'website_sale_order': order,
         })
         return value
+
+    @http.route(['/shop/cart/update'], type='http', auth="public",
+                methods=['POST'], website=True)
+    def cart_update(self, product_id, add_qty=1, set_qty=0, **kw):
+        """This route is called when adding a product to cart (no options)."""
+        sale_order = request.website.sale_get_order(force_create=True)
+        if sale_order.state != 'draft':
+            request.session['sale_order_id'] = None
+            sale_order = request.website.sale_get_order(force_create=True)
+
+        print(sale_order)
+        settings_product_data = request.env['res.config.settings']
+        product_bom_data = settings_product_data.search([])
+        for rec in product_bom_data:
+            # print(rec.product_ids)
+            settings_product_ids = rec.product_ids
+        # print(settings_product_ids)
+        bom_product_data = request.env['mrp.bom']
+        bom_records = bom_product_data.search([])
+        bom_meterials = []
+        for rec in bom_records:
+            pass
+            # print(rec.product_tmpl_id,int(product_id))
+        #     for data in rec.bom_line_ids:
+        #         # print(data.product_id.name)
+        #         bom_meterials.append(data.product_id.name)
+        # print(bom_meterials,product_id)
+
+
+
+
+        product_custom_attribute_values = None
+        if kw.get('product_custom_attribute_values'):
+            product_custom_attribute_values = json.loads(
+                kw.get('product_custom_attribute_values'))
+
+        no_variant_attribute_values = None
+        if kw.get('no_variant_attribute_values'):
+            no_variant_attribute_values = json.loads(
+                kw.get('no_variant_attribute_values'))
+
+        sale_order._cart_update(
+            product_id=int(product_id),
+            add_qty=add_qty,
+            set_qty=set_qty,
+            product_custom_attribute_values=product_custom_attribute_values,
+            no_variant_attribute_values=no_variant_attribute_values
+        )
+
+        if kw.get('express'):
+            return request.redirect("/shop/checkout?express=1")
+
+        return request.redirect("/shop/cart")
--- website_bom_in_cart/models/website_config_settings.py
+++ website_bom_in_cart/models/website_config_settings.py
@@ -1,13 +1,32 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models, tools
+from  ast import literal_eval
 from odoo.osv import expression
 from odoo.exceptions import UserError, ValidationError
 
 
-class DiscountLimit(models.TransientModel):
+class BillofMaterialsProduct(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    is_product = fields.Boolean(string='Products')
+    is_product = fields.Boolean(string='Products',
+                                config_parameter='website_bom_in_cart.is_product')
     product_ids = fields.Many2many('product.template', string="Products")
 
+    # product_ids = fields.Integer(string="Products")
+    def set_values(self):
+        res = super(BillofMaterialsProduct, self).set_values()
+        self.env['ir.config_parameter'].set_param(
+            'website_bom_in_cart.product_ids',
+            self.product_ids.ids)
+        # print(self.product_ids.ids)
+        return res
 
+    @api.model
+    def get_values(self):
+        res = super(BillofMaterialsProduct, self).get_values()
+        # select_type = self.env['ir.config_parameter'].sudo()
+        update_val = self.env['ir.config_parameter'].sudo().get_param(
+            'website_bom_in_cart.product_ids')
+        res.update({'product_ids': [(6, 0, literal_eval(update_val))]})
+        # print(update_val)
+        return res
